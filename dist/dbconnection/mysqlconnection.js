71894bdc13c2b92a313137bc3be5202e456907b7
Tejas01-developes <tlimbad7@gmail.com>
2026-04-09 23:58:31 +0530

    update

--- dist/dbconnection/mysqlconnection.js
+++ dist/dbconnection/mysqlconnection.js
@@ -1,10 +1,16 @@
 import mysql from 'mysql2/promise';
 import * as dotenv from 'dotenv';
 dotenv.config();
-const { MYSQL_HOST, MYSQL_PORT, MYSQL_DB, MYSQL_PASS, MYSQL_ROOT } = process.env;
-if (!MYSQL_DB || !MYSQL_HOST || !MYSQL_PASS || !MYSQL_PORT || !MYSQL_ROOT) {
-    throw Error("no process env found");
-}
+// const{
+//     MYSQL_HOST,
+//     MYSQL_PORT,
+//     MYSQL_DB,
+//     MYSQL_PASS,
+//     MYSQL_ROOT
+// }=process.env;
+// if(!MYSQL_DB || !MYSQL_HOST || !MYSQL_PASS || !MYSQL_PORT || !MYSQL_ROOT){
+//     throw Error("no process env found")
+// }
 export const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
     port: Number(process.env.MYSQL_PORT),
